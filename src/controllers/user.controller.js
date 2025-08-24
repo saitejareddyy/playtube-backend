339b0d70ecce5c6eb2e5c045b5de2173c51f7592
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-25 02:23:37 +0530

    fixed all bugs of register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,7 +31,12 @@ const registerUser = asyncHandler( async (req, res) => {
     console.log(req.files);
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if(!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
 
@@ -53,7 +58,7 @@ const registerUser = asyncHandler( async (req, res) => {
     "-password -refreshToken"
   )
 
-  if(createduser) throw new ApiError(500, "Something went wrong while registering user")
+  if(!createduser) throw new ApiError(500, "Something went wrong while registering user")
 
   return res.status(201).json(
     new ApiResponse(200, createduser, "User registered successfully")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,9 +15,10 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto"
     })
     console.log("File is uploaded on Cloudinary ", response.url)
+    fs.unlinkSync(localFilePath);
     return response
   } catch (error) {
-    fs.unlinkSync(localFilePath)  // remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath)  
     return null
   } 
 }
